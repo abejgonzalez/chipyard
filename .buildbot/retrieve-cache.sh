06e9492e208145f6c31089d085ae3b512edb46cf
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-04 18:08:59 -0700

    update

--- .buildbot/retrieve-cache.sh
+++ .buildbot/retrieve-cache.sh
@@ -10,12 +10,13 @@ HASH_FILE=$HOME/$2
 INSTALL_DIR=$HOME/$3
 
 # full cached name
-CACHE_NAME=$PREFIX-$(md5sum $HASH_FILE | awk '{print $1;}')
+CACHE_NAME=$PREFIX-$(md5sum $HASH_FILE | awk '{print $1;}').tar.gz
 
-if [ ! -d "$BB_SHARE_DOCKER_DIR/$CACHE_NAME" ]; then
+if [ ! -f "$BB_SHARE_DOCKER_DIR/$CACHE_NAME" ]; then
     echo "Failed to get $CACHE_NAME from the cache"
 else
     echo "Getting cache and moving it to $INSTALL_DIR"
-    cp -R $BB_SHARE_DOCKER_DIR/$CACHE_NAME $INSTALL_DIR
+    cp -R $BB_SHARE_DOCKER_DIR/$CACHE_NAME $INSTALL_DIR/..
+    tar -xzvf $INSTALL_DIR/../$CACHE_NAME
     echo "Done moving cache"
 fi
--- .buildbot/save-cache.sh
+++ .buildbot/save-cache.sh
@@ -10,13 +10,12 @@ HASH_FILE=$HOME/$2
 INSTALL_DIR=$HOME/$3
 
 # full cached name
-CACHE_NAME=$PREFIX-$(md5sum $HASH_FILE | awk '{print $1;}')
+CACHE_NAME=$PREFIX-$(md5sum $HASH_FILE | awk '{print $1;}').tar.gz
 
 if [ ! -d "$BB_SHARE_DOCKER_DIR/$CACHE_NAME" ]; then
     echo "Saving $CACHE_NAME"
     sudo chown -R buildbot:buildbot $BB_SHARE_DOCKER_DIR
-    mkdir -p $BB_SHARE_DOCKER_DIR/$CACHE_NAME
-    cp -R $INSTALL_DIR $BB_SHARE_DOCKER_DIR/$CACHE_NAME
+    tar -cvzf $BB_SHARE_DOCKER_DIR/$CACHE_NAME $INSTALL_DIR/
     echo "Done saving cache"
 else
     echo "$CACHE_NAME already exists. Skipping"
